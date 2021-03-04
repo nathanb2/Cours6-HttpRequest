e626c1c992ecdc8abb290f90fdc7f1933b1a15e0
Nathan <nathanb@ravtech.co.il>
2021-03-04 17:32:57 +0200

    add okHttpInterceptors

--- app/src/main/java/com/example/cours6_httprequest/RetrofitHelper.java
+++ app/src/main/java/com/example/cours6_httprequest/RetrofitHelper.java
@@ -1,16 +1,44 @@
 package com.example.cours6_httprequest;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitHelper {
 
-    private static Retrofit retrofit = new Retrofit.Builder()
+    private static final HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY);
+
+    private static Interceptor headerInterceptor = new Interceptor() {
+        @NotNull
+        @Override
+        public Response intercept(@NotNull Chain chain) throws IOException {
+            Request request = chain.request().newBuilder().addHeader("key", "value").build();
+            return chain.proceed(request);
+        }
+    };
+
+    private static final OkHttpClient okHttpClient = new OkHttpClient()
+            .newBuilder()
+            .addInterceptor(headerInterceptor)
+            .addInterceptor(loggingInterceptor)
+            .build();
+
+    private static final Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("https://api.github.com/")
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build();
 
-    private static APIService service = retrofit.create(APIService.class);
+    private static final APIService service = retrofit.create(APIService.class);
 
     public static APIService getRetrofitApiService() {
         return service;
